373a8a1edaa8d16d2c53b2802b05d8a1865d5be2
Felipe Silva <amorimdub@gmail.com>
2023-08-03 01:09:04 +0100

    add open ai

--- config/database.js
+++ config/database.js
@@ -1,14 +1,37 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
-      ssl: env.bool(true),
+const path = require("path");
+
+module.exports = ({ env }) => {
+  const client = env("DATABASE_CLIENT", "sqlite");
+
+  const connections = {
+    postgres: {
+      connection: {
+        connectionString: env("DATABASE_URL"),
+        schema: env("DATABASE_SCHEMA", "public"),
+      },
+      pool: {
+        min: env.int("DATABASE_POOL_MIN", 2),
+        max: env.int("DATABASE_POOL_MAX", 10),
+      },
     },
-    pool: { min: 0 }
-  },
-});
+    sqlite: {
+      connection: {
+        filename: path.join(
+          __dirname,
+          "..",
+          env("DATABASE_FILENAME", "data.db")
+        ),
+      },
+      useNullAsDefault: true,
+    },
+  };
+  console.log("client", client);
+  console.log("connections", connections[client]);
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+    },
+  };
+};
